76f6e4d8ff866c65c7e592b3998bba96e485d875
David Spector <davidspector08@gmail.com>
2020-04-13 23:24:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,13 +15,24 @@ end
 
 def get_english_meaning(file_path, japanese_emoticon)
 library = load_library(file_path)
-library.each do |key, hash|
+library.each do |meaning, hash|
   if hash[:japanese] == japanese_emoticon
+    return meaning
+  end
+end
+    "Sorry, that emoticon was not found"
+end	
+  
+def get_japanese_emoticon(file_path, english_emoticon)
+library = load_library(file_path)
+library.each do |meaning, hash|
+  if hash[:english] == english_emoticon
     return key
   end
 end
     "Sorry, that emoticon was not found"
 end	
+  
     
 def get_japanese_emoticon(file_path, english_emoticon)
 library = load_library(file_path)
